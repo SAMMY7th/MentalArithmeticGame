42c470d974cd1b7f6077145dec4c2d54628eb01b
Abe Asami <sammy@nocono.net>
2021-08-19 19:26:18 +0900

    コンソールアプリケーションとして作ることにしたので必要な依存ライブラリ追加

---
+++ src/main/java/net/nocono/mentalarithmetic/Application.java
@@ -0,0 +1,19 @@
+package net.nocono.mentalarithmetic;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements ApplicationRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // TODO
+    }
+}
