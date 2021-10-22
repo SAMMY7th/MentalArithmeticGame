10499b0f4517b47a149a404e7b41fd7529f283a1
Abe Asami <sammy@nocono.net>
2021-10-22 23:59:55 +0900

    演算子の優先順位が不要になったので廃止

--- src/main/java/net/nocono/mentalarithmetic/domain/model/expression/token/Operator.java
+++ src/main/java/net/nocono/mentalarithmetic/domain/model/expression/token/Operator.java
@@ -6,34 +6,28 @@ import java.util.Random;
  * 演算子
  */
 public enum Operator implements Token {
-    足す("+", new Priority(2)) {
+    足す("+") {
         public double apply(double value1, double value2) { return value1 + value2; }
     },
-    引く("-", new Priority(2)) {
+    引く("-") {
         public double apply(double value1, double value2) { return value1 - value2; }
     },
-    掛ける("*", new Priority(1)) {
+    掛ける("*") {
         public double apply(double value1, double value2) { return value1 * value2; }
     },
-    割る("/", new Priority(1)) {
+    割る("/") {
         public double apply(double value1, double value2) { return value1 / value2; }
     };
 
     String operator;
-    Priority priority;
 
-    Operator(String operator, Priority priority) {
+    Operator(String operator) {
         this.operator = operator;
-        this.priority = priority;
     }
 
     public abstract double apply(double value1, double value2);
 
     static Random random = new Random();
-
-    public boolean の方がターゲットより優先順位が高い(Operator target) {
-        return this.priority.の方がターゲットより優先順位が高い(target.priority);
-    }
 
     public static Operator random() {
         int nextInt = random.nextInt(4);
